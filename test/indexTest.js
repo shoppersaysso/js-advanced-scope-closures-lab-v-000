e9d7fa8daa4e52b54d46c3196317c0820910c0c0
Sara Hopper <sara.hopper@gmail.com>
2017-10-01 17:36:18 +0000

    automatically backed up by learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -33,13 +33,13 @@ describe('closures', function() {
 
   // describe('createDriver', function(){
   //   const Driver = createDriver()
-  // 
+  //
   //   it('returns a class that allows us to create a driver with a name attribute', function(){
   //     expect(new Driver('Sam').name).to.equal('Sam')
   //   })
-  // 
+  //
   //   it('increments the driverId each time a driver is created', function(){
   //     expect(new Driver('Sam').id).to.not.equal(new Driver('Bob').id)
   //   })
-  })
+  // })
 })
